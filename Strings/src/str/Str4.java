40ef553c7d840f6fd76a1e3d0108567f9a02933f
Akhilan <Akhilan@DESKTOP-BAGKLH4>
2021-05-13 11:01:47 +0530

    commit

---
+++ Strings/src/str/Str4.java
@@ -0,0 +1,10 @@
+package str;
+import java.util.*;
+public class Str4 {
+	public static void main(String[] args)
+	 {
+		Scanner sc=new Scanner(System.in);
+	    String str = sc.nextLine();    
+	    System.out.println(str.substring(0, str.length()/2));	
+	  } 
+}
