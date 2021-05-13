dce64f7de6b1646ffe3e7aa7c026597a589925f8
Akhilan <Akhilan@DESKTOP-BAGKLH4>
2021-05-13 10:48:43 +0530

    commit

---
+++ LOOPS/src/crt/Pgm10.java
@@ -0,0 +1,12 @@
+package crt;
+public class Pgm10 {
+	public static void main(String[] args)
+    {
+    			for (int i = 1; i <= 10; i++) {
+    		   
+    		   if (i>=1) {
+    			System.out.print(i + " ");
+    		   }
+    			}
+    }
+}
---
+++ LOOPS/src/crt/Pgm2.java
@@ -0,0 +1,20 @@
+package crt;
+import java.util.Scanner;
+
+public class Pgm2 {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("the number is "+a +" even");
+		}
+		else
+		{
+			System.out.println("the number is "+a +" odd");
+		}
+		
+}
+	
+}
